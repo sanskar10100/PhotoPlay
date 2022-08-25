dfc08a6454e3b3f80bcd1167592036371fbaf0f0
Sanskar Agrawal <sanskar10100@gmail.com>
2022-08-25 13:20:33 +0530

    Add basic watchlist fragment

--- app/src/main/java/dev/sanskar/photoplay/ui/watchlist/WatchlistFragment.kt
+++ app/src/main/java/dev/sanskar/photoplay/ui/watchlist/WatchlistFragment.kt
@@ -1,7 +1,35 @@
 package dev.sanskar.photoplay.ui.watchlist
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.fragment.app.Fragment
+import dev.sanskar.photoplay.ui.theme.PhotoPlayTheme
 
 class WatchlistFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return ComposeView(requireContext()).apply {
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
+                PhotoPlayTheme {
+                    WatchlistScreen()
+                }
+            }
+        }
+    }
 
+    @Composable
+    fun WatchlistScreen(modifier: Modifier = Modifier) {
+        Text(text = "Hello, world")
+    }
 }
