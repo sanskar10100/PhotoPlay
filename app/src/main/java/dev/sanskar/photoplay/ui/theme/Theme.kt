5cd9b2d9eeaf48d3213f5bb6b047b8cfeeac84fe
Sanskar Agrawal <sanskar10100@gmail.com>
2022-08-29 19:06:39 +0530

    Disable dark theme

--- app/src/main/java/dev/sanskar/photoplay/ui/theme/Theme.kt
+++ app/src/main/java/dev/sanskar/photoplay/ui/theme/Theme.kt
@@ -1,16 +1,8 @@
 package dev.sanskar.photoplay.ui.theme
 
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
-
-private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
-)
 
 private val LightColorPalette = lightColors(
     primary = Purple500,
@@ -28,15 +20,9 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun PhotoPlayTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
-
+fun PhotoPlayTheme(content: @Composable () -> Unit) {
     MaterialTheme(
-        colors = colors,
+        colors = LightColorPalette,
         typography = Typography,
         shapes = Shapes,
         content = content
